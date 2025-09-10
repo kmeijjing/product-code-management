677a561c8e64a334c2c218a393ac3a1138ee4025
MeeKyeong Kim <kmeijjing@gmail.com>
2025-09-10 13:19:11 +0900

    ✨ VSCode 설정 및 패키지 업데이트: Prettier 및 Tailwind CSS 플러그인 추가, 코드 포맷팅 옵션 설정

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
+	reactStrictMode: false,
+	swcMinify: true,
 };
 
 export default nextConfig;
-
